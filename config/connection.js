e4eed4cd29ad0d004106d971880506ecb1b3f60f
elfsvet <stepanmatysik@gmail.com>
2022-06-05 00:14:54 -0400

    did models next is routes

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,10 @@
+
+// import the Sequelize constructor from the library
+const Sequelize = require('sequelize');
+// to use environment variables to store sensitive data
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
-
+// if 
 const sequelize = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
