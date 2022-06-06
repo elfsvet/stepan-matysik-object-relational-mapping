ea7ff1c669f693fbb23e9876b7d33deecfc05687
elfsvet <stepanmatysik@gmail.com>
2022-06-06 01:00:25 -0400

    added instruction for the video recording. Need to be done tomorrow in the morning and it's ready to be submitted

--- server.js
+++ server.js
@@ -20,3 +20,20 @@ sequelize.sync({force: false}).then(() => {
 });
 // i also created heroku server, but you would need to seed by itself.
 // https://mysterious-harbor-32872.herokuapp.com/
+
+// TODO record a video!
+/*
+The walkthrough video must show all of the technical acceptance criteria being met.
+
+The walkthrough video must demonstrate how to create the schema from the MySQL shell.
+
+The walkthrough video must demonstrate how to seed the database from the command line.
+
+The walkthrough video must demonstrate how to start the application’s server.
+
+The walkthrough video must demonstrate GET routes for all categories, all products, and all tags being tested in Insomnia.
+
+The walkthrough video must demonstrate GET routes for a single category, a single product, and a single tag being tested in Insomnia.
+
+The walkthrough video must demonstrate POST, PUT, and DELETE routes for categories, products, and tags being tested in Insomnia.
+*/
